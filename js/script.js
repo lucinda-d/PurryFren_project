facf5c49a6bc5c51c809042ff89ce626002643ec
Lucy D <126295672+lucinda-d@users.noreply.github.com>
2024-01-29 10:09:34 -0600

    Added some button text

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 // console.log(cat);
 
-button.addEventListener("click", function () {
-    // console.log("its working");
+button.addEventListener("click", function () 
+{
+   if (cat.classList.contains("show")) {
+    // console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+   } 
+   else {
+    // console.log("No");
     cat.classList.add("show");
-})
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+   }
+
+});
+
